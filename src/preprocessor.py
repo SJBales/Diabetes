d379867b3ccbd9aa9f4ca2c2fe5e328e79ada854
balessj <sbales556@gmail.com>
2026-06-19 09:32:27 -0500

    Updates to the preprocessor class to add logging and error handling. Creation of a an inference proprocessor to handle preprocessing when deployed to production.

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,34 +1,68 @@
 import pandas as pd
+import logging
+import warnings
 
+# Local module imports
+from src.logger_config import setup_logger
+from exceptions import NotProcessedError
+
+# Sklearn Imports
+from sklearn.model_selection import train_test_split
+
+# Setting up the logger
+setup_logger()
+logging.captureWarnings(True)
+
+logger = logging.getLogger(__name__)
+
+
+# Function for loading data
 def load_data():
 
     features = pd.read_parquet("data/diabetes_features.parquet")
     target = pd.read_parquet("data/diabetes_target.parquet")
-    
+
+    logger.info("Successfully Loaded Data")
+
     return features, target
 
 
-class dataPreprocessor:
+# Defining a class for handling all preprocessing steps
+class trainingPreprocessor:
 
     def __init__(self, features, target) -> None:
-        self.raw_fetaures = features
+        self.raw_features = features
         self.raw_target = target
         self.split_df = None
         self.X = None
         self.y = None
+        self._missing_processed_ = None
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
 
     def first_encounter(self) -> None:
         '''Limits the input dataframe to the first enounter of the patient'''
 
         # Creating a dataframe for selecting first encounters
-        splitting_df = features.copy()
-        splitting_df['target'] = (target['readmitted'] == "<30").astype(int)
+        splitting_df = self.raw_features.copy()
+        splitting_df['target'] = (self.raw_target['readmitted'] == "<30").astype(int)
 
         # Keeping only the first encounter per patient_nbr
-        self.split_df = splitting_df.drop_duplicates('patient_nbr', keep='first')
+        self.split_df = splitting_df.drop_duplicates('patient_nbr',
+                                                     keep='first')
+
+        logger.info("Successfully subset to the first encounter")
 
     def handle_missings(self) -> None:
         '''Handles preprocessing of missing data'''
+
+        # Checking that the split DF has been created
+        if self.split_df is None:
+            logger.info("Data has not been limited to first encounter, running method")
+
+            self.first_encounter()
 
         # Creating a feature to indicate if a patient is not on insulin or metformin
         self.split_df['missing_insulin'] = (self.split_df.insulin == "No").astype(int)
@@ -64,38 +98,148 @@ class dataPreprocessor:
         self.split_df['missing_a1c'] = (self.split_df['A1Cresult'] == "None").astype("int")
         self.split_df['missing_max_glu_serum'] = (self.split_df['max_glu_serum'] == "None").astype("int")
 
-    def create_train_test(self):
+        # Updating attribute to flag processing has been completed
+        self._missing_processed_ = True
+
+        # Logging completed work
+        logger.info("Successfully processed missing data")
+
+    def create_X_y(self):
+        '''Creates X and y dataframes for modeling'''
+
+        # Checking that missing data has been processed
+        if not self._missing_processed_:
+            warnings.warn("Missing data has not been processed, running method")
+            self.handle_missings()
+
         # List of features to drop
         drop_features = ['patient_nbr', 'encounter_id', 'target',
-                        'repaglinide', 'nateglinide', 'chlorpropamide',
-                        'glimepiride', 'acetohexamide', 'glipizide',
-                        'glyburide', 'tolbutamide',
-                        'pioglitazone', 'rosiglitazone', 'acarbose', 'miglitol', 'troglitazone',
-                        'tolazamide', 'examide', 'citoglipton', 'insulin',
-                        'glyburide.metformin', 'glipizide.metformin',
-                        'glimepiride.pioglitazone', 'metformin.rosiglitazone',
-                        'metformin.pioglitazone', 'metformin', 'insulin',
-                        'A1Cresult', 'weight', 'diag_1', 'diag_2', 'diag_3',
-                        'max_glu_serum', 'change']
+                         'repaglinide', 'nateglinide', 'chlorpropamide',
+                         'glimepiride', 'acetohexamide', 'glipizide',
+                         'glyburide', 'tolbutamide',
+                         'pioglitazone', 'rosiglitazone', 'acarbose',
+                         'miglitol', 'troglitazone',
+                         'tolazamide', 'examide', 'citoglipton', 'insulin',
+                         'glyburide.metformin', 'glipizide.metformin',
+                         'glimepiride.pioglitazone', 'metformin.rosiglitazone',
+                         'metformin.pioglitazone', 'metformin', 'insulin',
+                         'A1Cresult', 'weight', 'diag_1', 'diag_2', 'diag_3',
+                         'max_glu_serum', 'change']
 
         # Building X & y dataframes
         self.y = self.split_df['target']
         self.X = self.split_df.drop(columns=drop_features)
 
-        # Creating train and test splits
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,
-                                                                                self.y,
-                                                                                random_state=42,
-                                                                                test_size=0.2)
-    def clean(self):
+        # Logging completed work
+        logger.info("Successfully created X & y dataframes")
+
+    def create_train_test(self, return_df=False, test_size_=0.2) -> pd.Dataframe | None:
+        '''Creates train and test splits for modeling'''
+
+        if return_df:
+            logger.info("Returning train and test splits of X & y")
+            return train_test_split(self.X,
+                                    self.y,
+                                    random_state=42,
+                                    test_size=test_size_)
+        else:
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,
+                                                                                    self.y,
+                                                                                    random_state=42,
+                                                                                    test_size=test_size_)
+            logger.info("Assigning X and y splits as attributes")
+
+    def clean(self, return_df=False, test_size_=0.2):
         '''Master method for running all cleaning methods'''
+
         self.first_encounter()
         self.handle_missings()
-        self.create_train_test()
+        self.create_X_y()
+        self.create_train_test(return_df=return_df, test_size_=test_size_)
 
-if __name__ == "__main__":
-    features, target = load_data()
+        # Logging status
+        logger.info("Successfully completed all cleaning steps")
 
-    preprocessor_engine = dataPreprocessor()
 
-    dataPreprocessor.clean()
+class inferencePreprocessor:
+
+    def __init__(self, features) -> None:
+        self.raw_features = features
+        self.missing_processed_ = False
+        self.split_df = None
+        self.X = None
+
+    def first_encounter(self) -> None:
+        '''Limits the input dataframe to the first enounter of the patient'''
+
+        # Keeping only the first encounter per patient_nbr
+        self.split_df = self.raw_features.drop_duplicates('patient_nbr',
+                                                          keep='first')
+
+    def handle_missings(self) -> None:
+        '''Handles preprocessing of missing data'''
+
+        # Checking that missing data has been processed for production, raises exception if not
+        if not self.missing_processed_:
+            raise NotProcessedError("Missing data has not been processed")
+
+        # Creating a feature to indicate if a patient is not on insulin or metformin
+        self.split_df['missing_insulin'] = (self.split_df.insulin == "No").astype(int)
+        self.split_df['missing_metformin'] = (self.split_df.metformin == "No").astype(int)
+
+        # Creating a dictionary to encode changes in insulin and metformin
+        encode_dict = {'No': 0,
+                       "Steady": 0,
+                       "Up": 1,
+                       "Down": -1}
+
+        # Applying the encoding to engineer new features
+        self.split_df['metformin_change'] = self.split_df['metformin'].apply(lambda x: encode_dict[x])
+        self.split_df['insulin_change'] = self.split_df['insulin'].apply(lambda x: encode_dict[x])
+
+        # Encoding Missing Weight Values
+        self.split_df['missing_weight'] = (self.split_df['weight'] == "?").astype("int")
+
+        # Utility function for encoding missing values in a column for one-hot encoding
+        def missing_cleaner(x, missing_code, encoding=""):
+            if x == missing_code:
+                return encoding
+            else:
+                return x
+
+        # Encoding Missing Payer Code and Medical Specialty
+        self.split_df['payer_code_cleaned'] = self.split_df['payer_code']
+
+    def create_X(self, return_df=False):
+        '''Creates X dataframe for modeling'''
+
+        # List of features to drop
+        drop_features = ['patient_nbr', 'encounter_id', 'target',
+                         'repaglinide', 'nateglinide', 'chlorpropamide',
+                         'glimepiride', 'acetohexamide', 'glipizide',
+                         'glyburide', 'tolbutamide',
+                         'pioglitazone', 'rosiglitazone', 'acarbose',
+                         'miglitol', 'troglitazone',
+                         'tolazamide', 'examide', 'citoglipton', 'insulin',
+                         'glyburide.metformin', 'glipizide.metformin',
+                         'glimepiride.pioglitazone', 'metformin.rosiglitazone',
+                         'metformin.pioglitazone', 'metformin', 'insulin',
+                         'A1Cresult', 'weight', 'diag_1', 'diag_2', 'diag_3',
+                         'max_glu_serum', 'change']
+
+        if return_df:
+            return self.split_df.drop(columns=drop_features)
+        else:
+            # Building X dataframe
+            self.X = self.split_df.drop(columns=drop_features)
+
+    def clean(self, return_df):
+
+        try:
+            self.first_encounter()
+            self.handle_missings()
+            self.create_X(return_df=return_df)
+
+        except NotProcessedError:
+            logger.exception("Pipeline step called out of order")
+            raise
