47dd68a2f2e24283646b9373b4c876186f30202f
balessj <sbales556@gmail.com>
2026-06-21 08:22:51 -0500

    Addition of the module import error to exceptions and update of local module import commands in logger

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,7 @@
 # Class to raise exception that the data has not been processed
 class NotProcessedError(RuntimeError):
     pass
+
+
+class ModelError(ValueError):
+    pass
--- src/logger_config.py
+++ src/logger_config.py
@@ -3,5 +3,8 @@ import logging
 
 def setup_logger(level=logging.INFO):
     logging.basicConfig(level=level,
-                        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+                        format="""%(asctime)s |
+                        %(levelname)s |
+                        %(name)s |
+                        %(message)s""",
                         datefmt="%Y-%m-%d %H:%M:%S")
