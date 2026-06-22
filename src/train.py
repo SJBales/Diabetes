d7538673f6ca95697b75a5baf13ad384c079b9ad
balessj <sbales556@gmail.com>
2026-06-22 06:29:17 -0500

    Bug fixes for a successful run of the training pipeline

--- src/train.py
+++ src/train.py
@@ -1,14 +1,27 @@
 from preprocessor import load_data, trainingPreprocessor
 from pipeline import getPipeline
+import logging
+
+# Setting up the logger
+from logger_config import setup_logger
+
+setup_logger()
+logger = logging.getLogger(__name__)
+
+# ------------------ Loading Data ------------------
 
 # Loading the data
 features, target = load_data()
+
+# ------------------ Running the Preprocessor ------------------
 
 # Setting up the training preprocessor
 preprocessor = trainingPreprocessor(features, target)
 
 # Running the cleaning steps
-X_train, X_test, y_train, y_test = preprocessor.clean()
+X_train, X_test, y_train, y_test = preprocessor.clean(return_df=True)
+
+# ------------------ Training the Model ------------------
 
 # Defining optimal parameters
 optimal_hypers = {
@@ -20,6 +33,14 @@ optimal_hypers = {
 }
 
 # Setting up the pipeline
-training_pipeline = getPipeline(optimal_hypers,
-                                model="RF",
-                                return_pipeline=True)
+training_pipeline = getPipeline().make_training_pipeline(model="RF",
+                                                         return_pipeline=True,
+                                                         **optimal_hypers)
+
+logger.info("Successfully created training pipeline")
+
+# Fitting the pipeline
+training_pipeline.fit(X_train, y_train)
+
+logger.info("Successfully fit model on training data")
+
