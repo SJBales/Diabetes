0ac425f0dd2d0db002fd2ccb4c36c67d6941bd14
balessj <sbales556@gmail.com>
2026-06-26 06:51:34 -0500

    Added wrapper function for standalone implementation of the training steps for model deployment.

--- src/train.py
+++ src/train.py
@@ -9,45 +9,57 @@ from logger_config import setup_logger
 setup_logger()
 logger = logging.getLogger(__name__)
 
-# ------------------ Loading Data ------------------
 
-# Loading the data
-features, target = load_data()
+def train_model() -> None:
 
-# ------------------ Running the Preprocessor ------------------
+    '''Self contained function to train the model and
+    save it in binary format'''
+    # ------------------ Loading Data ------------------
 
-# Setting up the training preprocessor
-preprocessor = trainingPreprocessor(features, target)
+    # Loading the data
+    features, target = load_data()
 
-# Running the cleaning steps
-X_train, X_test, y_train, y_test = preprocessor.clean(return_df=True)
+    # ------------------ Running the Preprocessor ------------------
 
-# ------------------ Training the Model ------------------
+    # Setting up the training preprocessor
+    preprocessor = trainingPreprocessor(features, target)
 
-# Defining optimal parameters
-optimal_hypers = {
-    'max_depth': 5,
-    'min_samples_split': 10,
-    'n_estimators': 200,
-    'max_features': 0.6,
-    'class_weight': 'balanced'
-}
+    # Running the cleaning steps
+    X_train, X_test, y_train, y_test = preprocessor.clean(return_df=True)
 
-# Setting up the pipeline
-training_pipeline = getPipeline().make_training_pipeline(model="RF",
-                                                         return_pipeline=True,
-                                                         **optimal_hypers)
+    # ------------------ Training the Model ------------------
 
-logger.info("Successfully created training pipeline")
+    # Defining optimal parameters
+    optimal_hypers = {
+        'max_depth': 5,
+        'min_samples_split': 10,
+        'n_estimators': 200,
+        'max_features': 0.6,
+        'class_weight': 'balanced'
+    }
 
-# Fitting the pipeline
-training_pipeline.fit(X_train, y_train)
+    # Setting up the pipeline
+    training_pipeline = getPipeline().make_training_pipeline(
+        model="RF",
+        return_pipeline=True,
+        **optimal_hypers)
 
-logger.info("Successfully fit model on training data")
+    logger.info("Successfully created training pipeline")
 
-# Evaluating performance
-log_performance(training_pipeline, X_train, y_train)
-log_performance(training_pipeline, X_test, y_test, kind="TEST")
+    # Fitting the pipeline
+    training_pipeline.fit(X_train, y_train)
 
-# Saving the model
-joblib.dump(training_pipeline, "models/RF_v1.0.pkl")
+    logger.info("Successfully fit model on training data")
+
+    # ------------------ Evaluating Performance ------------------
+
+    # Evaluating performance
+    log_performance(training_pipeline, X_train, y_train)
+    log_performance(training_pipeline, X_test, y_test, kind="TEST")
+
+    # ------------------ Saving The Model ------------------
+    joblib.dump(training_pipeline, "models/RF_v1.0.pkl")
+
+
+if __name__ == "__main__":
+    train_model()
