874ff5b83b15363fd306489be1e0cc6f2b2eb408
balessj <sbales556@gmail.com>
2026-07-09 06:35:26 -0500

    Initializing a test script for the pipeline method

---
+++ tests/test_pipeline.py
@@ -0,0 +1,7 @@
+# Plan
+'''
+One test to validate the CT is created
+One test to validate the training pipeline is created
+    - Sub test for wrong module usage
+One test to test logging of model performance
+'''
